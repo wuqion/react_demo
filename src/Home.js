f7e90faced0a52715fe593c01d883ef7757ee245
吴琼 <wuqiongaia@163.com>
2020-04-04 22:57:01 +0800

    样式的使用方式

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,19 @@
 import React from 'react';
-
+import './css/common.css';
+import classNames from 'classnames';
+const styles = {
+    color:'#090'
+}
 function Home (){
+    
     return(
-        <div>函数式创建模块</div>
+        <div>
+             <h1 style={{color:"red"}}>内连样式1</h1>
+             <h1 style={styles}>内连样式2</h1>
+             <h1 className='text-blueviolet'>class样式</h1>
+             <h1 className={classNames('text-blueviolet', { textfont20: true, textfont10: false })}>classnames动态方式样式</h1>
+
+            </div>
     )
 }
 export default Home;
